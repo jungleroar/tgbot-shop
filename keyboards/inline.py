aeadef7d3a7511a48bfe440ddcb4e7386d631e4f
onemanzerg <donnydeus@gmail.com>
2025-03-19 23:50:30 +0300

    add callback_queryes

--- keyboards/inline.py
+++ keyboards/inline.py
@@ -12,3 +12,31 @@ def get_callback_btns(
     for text, data in btns.items():
         keyboard.add(InlineKeyboardButton(text=text, callback_data=data))
     return keyboard.adjust(*sizes).as_markup()
+
+
+def get_url_btns(
+    *,
+    btns: dict[str, str],
+    sizes: tuple[int] = (2,)
+):
+    keyboard = InlineKeyboardBuilder()
+
+    for text, url in btns.items():
+        keyboard.add(InlineKeyboardButton(text=text, url=url))
+    return keyboard.adjust(*sizes).as_markup()
+
+
+def get_inlineMix_btns(
+    *,
+    btns: dict[str, str],
+    sizes: tuple[int] = (2,)
+):
+    keyboard = InlineKeyboardBuilder()
+
+    for text, value in btns.items():
+        if "://" in value:
+            keyboard.add(InlineKeyboardButton(text=text, url=value))
+        else:
+            keyboard.add(InlineKeyboardButton(text=text, callback_data=value))
+
+    return keyboard.adjust(*sizes).as_markup()
